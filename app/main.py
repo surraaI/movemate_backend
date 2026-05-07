0cea50e78950a2dabb4b646b83078981ea36e9e5
Sura Itana <141846629+surraaI@users.noreply.github.com>
2026-05-08 01:55:04 +0300

    Improve auth (#19)
    * fix pydantic circular import issues
    
    * improve auth, only commuters can register themselves, others only be created by admins or superadmin
    
    * add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.router import api_router
 from app.core.config import settings
@@ -28,6 +29,18 @@ def create_app() -> FastAPI:
         title=settings.PROJECT_NAME,
         version=settings.VERSION,
         lifespan=lifespan,
+    )
+    
+    origins = [
+        "http://localhost:54028",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,   # or ["*"] for all
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app.include_router(api_router, prefix="/api/v1")
