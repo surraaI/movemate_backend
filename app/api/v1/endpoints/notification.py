87564c51515b09860d826c9d6bb291669fef5261
Sura Itana <141846629+surraaI@users.noreply.github.com>
2026-05-07 16:27:29 +0300

    fix pydantic issue (#16)

--- app/api/v1/endpoints/notification.py
+++ app/api/v1/endpoints/notification.py
@@ -2,27 +2,27 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.services.notification_service import NotificationService
-from app.schemas.notification import NotificationCreate, NotificationOut
+from app.schemas.notification import NotificationCreate
 from app.models.notification import Notification
 from app.core.deps import get_current_user
 from app.models.user import User
 
 router = APIRouter()
 
-
-@router.post("/", response_model=NotificationOut)
+@router.post("/")
 def create_notification(data: NotificationCreate, db: Session = Depends(get_db)):
     return NotificationService.create(db, data)
 
 
-@router.get("/", response_model=list[NotificationOut])
+
+@router.get("/")
 def get_notifications(
-    db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db), 
+    current_user: User = Depends(get_current_user) # Logic runs automatically
 ):
+    # Use the authenticated user's ID
     return db.query(Notification).filter(Notification.user_id == current_user.user_id).all()
 
-
-@router.put("/{id}/read", response_model=NotificationOut)
+@router.put("/{id}/read")
 def mark_as_read(id: str, db: Session = Depends(get_db)):
     return NotificationService.mark_as_read(db, id)
